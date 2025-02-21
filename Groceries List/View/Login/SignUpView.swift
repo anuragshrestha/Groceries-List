d795e2495f54c718eaa1d32f0d86d2a281de057f
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-02-21 15:35:27 -0700

    initial Signup screen

--- Groceries List/View/Login/SignUpView.swift
+++ Groceries List/View/Login/SignUpView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @StateObject var mainVM = MainViewModel.shared;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+     
+            VStack{
+                
+                Text("Grocery Shopping App")
+                    .font(.customfont(.bold, fontSize: 24))
+                    .foregroundColor(.red.opacity(0.8))
+                    .padding(.top, .topInsets + 20)
+                
+                
+                ScrollView{
+                    
+                    VStack{
+                        LineTextField(text: $mainVM.userName, title: "User name", placeholder: "Enter your user name")
+                            .padding(.bottom, .screenWidth * 0.07)
+                        
+                        LineTextField(text: $mainVM.email, title: "Email", placeholder:"Enter your email", keyboardType: .emailAddress)
+                            .padding(.bottom, .screenWidth * 0.07)
+                        
+                        
+                        SecureLineField(text: $mainVM.password, title: "Password", placeholder: "Enter your password", isShowPassword: $mainVM.isShowPassword)
+                            .padding(.bottom, .screenWidth * 0.07)
+                        
+                        HStack{
+                            Text("By continuing you agree to our ")
+                                .font(.customfont(.semibold, fontSize: 15))
+                                .foregroundColor(.primaryText) +
+                            Text("terms of Service ")
+                                .font(.customfont(.semibold, fontSize: 15))
+                                .foregroundColor(.blue) +
+                            Text("and ")
+                                .font(.customfont(.semibold, fontSize: 15))
+                                .foregroundColor(.primaryText) +
+                            Text("Privacy Policy.")
+                                .font(.customfont(.semibold, fontSize: 15))
+                                .foregroundColor(.blue)
+                        }
+                        .padding(.bottom, .screenWidth * 0.07)
+                        
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, .bottomInsets)
+                }
+                .padding(.top, 40)
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(LinearGradient(
+                gradient: Gradient(colors: [.gray, .white]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+                .ignoresSafeArea()
+            )
+            .navigationTitle("")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+            .ignoresSafeArea()
+        
     }
 }
 
