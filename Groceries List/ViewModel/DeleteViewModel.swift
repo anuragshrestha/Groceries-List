eca257420c732900d27650eb22979cdc9848a100
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-03-29 13:05:37 -0600

    view model for deleting grocery

--- Groceries List/ViewModel/DeleteViewModel.swift
+++ Groceries List/ViewModel/DeleteViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class DeleteViewModel: ObservableObject{
+    
+    @Published var isLoading:Bool = false
+    @Published var errorMessage:String?
+    @Published var isDeleted:Bool = false
+    
+    private var deleteService  = DeleteService()
+    
+    func deleteGrocery(listId: String) {
+        
+      
+        if listId.isEmpty{
+            self.errorMessage = "List Id is empty!"
+            return
+        }
+        
+        self.isLoading = true
+        
+        deleteService.deleteGrocery(listId: listId) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result{
+                case .success(let message):
+                    print("Sucess \(message)")
+                    self?.isDeleted = true
+                case .failure(let error):
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
