e6854287419187eab8bbc5d3d0996ddffb75685c
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-03-29 18:50:41 -0600

    Vew Model for complete Grocery that checks if a grocery is successfully stored or failed

--- Groceries List/ViewModel/CompleteGroceryViewModel.swift
+++ Groceries List/ViewModel/CompleteGroceryViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class CompleteGroceryViewModel: ObservableObject {
+    
+    @Published var errorMessage: String?
+    @Published var isLoading: Bool = false
+    @Published var isComplete: Bool = false
+    
+    private var completeService = CompleteService()
+    
+    func completeGrocery(listId: String, groceryName: String, quantity: String, dueDate: Date){
+        
+        guard !listId.isEmpty || !groceryName.isEmpty || !quantity.isEmpty else {
+            self.errorMessage = "Please fill all the fields"
+            return
+        }
+        
+        let dateFromatter = DateFormatter()
+        dateFromatter.dateFormat = "yyyy-MM-dd"
+        let formatDate = dateFromatter.string(from: dueDate)
+        
+        let item = GroceryItem(groceryName: groceryName, quantity: quantity, dueDate: formatDate, listId: listId)
+        
+        isLoading = true
+        
+        completeService.completeService(item: item) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result {
+                case .success(let message):
+                    print("Successfully stored in History table \(message)")
+                    self?.isComplete = true
+                case .failure(let error):
+                    print("Failed to store in history table \(error)")
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
